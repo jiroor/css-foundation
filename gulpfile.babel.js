c208209507f2ec7ae8cad65c0408655e3bbb3843
jiroor <harryyotter@hotmail.com>
2015-10-05 18:57:01 +0900

    feat(mixin): add file of mixin for css3

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,7 @@ import gulp from 'gulp'
 import sass from 'gulp-sass'
 
 gulp.task('sass', () => {
-  gulp.src('index.sass')
+  gulp.src('./sass/index.sass')
     .pipe(sass())
     .pipe(gulp.dest('./dst'))
 })
